374f19a54e412d06af7b717b29a43716785b2f6e
Soujanya115 <soujanyarao115@gmail.com>
2025-05-23 22:40:46 +0530

    Examples on ex_01_Java_Basics_Part2

--- src/ex_01_Java_Basics_Par2/Lab023_LocalVariable.java
+++ src/ex_01_Java_Basics_Par2/Lab023_LocalVariable.java
@@ -8,8 +8,8 @@ public class Lab023_LocalVariable {
     }
 
     public static void main(int args) {
-        int score = 200;
-        System.out.println("Here is 'score' is local variable to this main method, but not to the above method");
+        int age = 10;
+        System.out.println("Here is 'age' is local variable to this main method, but not to the above method");
 
 
     }
